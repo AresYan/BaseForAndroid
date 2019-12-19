b421ae95668b06330edb87982e7bee396a9380ed
yz <yanzheng>
2019-12-19 16:01:01 +0800

    更新Toasty

--- base/src/main/java/com/yz/base/utils/MyToasty.java
+++ base/src/main/java/com/yz/base/utils/MyToasty.java
@@ -1,7 +1,6 @@
 package com.yz.base.utils;
 
 import android.content.Context;
-import android.widget.TextView;
 import android.widget.Toast;
 import es.dmoral.toasty.Toasty;
 
@@ -18,33 +17,7 @@ public class MyToasty {
         mContext=context;
     }
 
-	private static Toast mToast = null;
-
-	public static void toast(final String text) {
-	    if(mContext==null){
-	        return;
-        }
-		MyMainHandler.post(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					if (mToast == null) {
-						mToast = Toast.makeText(mContext, text, Toast.LENGTH_SHORT);
-					} else {
-						mToast.setText(text);
-						mToast.setDuration(Toast.LENGTH_SHORT);
-					}
-					mToast.show();
-				}catch (Exception e){
-				    e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	private static Toast mError = null;
-
-	public static void error(final String text){
+	public static void error(String text){
         if(mContext==null){
             return;
         }
@@ -52,22 +25,13 @@ public class MyToasty {
 			@Override
 			public void run() {
 				try {
-					if (mError == null) {
-						mError = Toasty.error(mContext,text, Toast.LENGTH_SHORT,true);
-					} else {
-						TextView textView = (TextView)mError.getView().findViewById(es.dmoral.toasty.R.id.toast_text);
-						textView.setText(text);
-						mError.setDuration(Toast.LENGTH_SHORT);
-					}
-					mError.show();
+					Toasty.error(mContext,text, Toast.LENGTH_SHORT,true).show();
 				}catch (Exception e){
 				    e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	private static Toast mInfo = null;
 
 	public static void info(final String text){
         if(mContext==null){
@@ -77,22 +41,13 @@ public class MyToasty {
 			@Override
 			public void run() {
 				try {
-					if (mInfo == null) {
-						mInfo = Toasty.info(mContext,text, Toast.LENGTH_SHORT,true);
-					} else {
-						TextView textView = (TextView)mInfo.getView().findViewById(es.dmoral.toasty.R.id.toast_text);
-						textView.setText(text);
-						mInfo.setDuration(Toast.LENGTH_SHORT);
-					}
-					mInfo.show();
+					Toasty.info(mContext,text, Toast.LENGTH_SHORT,true).show();
 				}catch (Exception e){
 				    e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	private static Toast mNormal = null;
 
 	public static void normal(final String text){
         if(mContext==null){
@@ -102,22 +57,13 @@ public class MyToasty {
 			@Override
 			public void run() {
 				try {
-					if (mNormal == null) {
-						mNormal = Toasty.normal(mContext,text, Toast.LENGTH_SHORT);
-					} else {
-						TextView textView = (TextView)mNormal.getView().findViewById(es.dmoral.toasty.R.id.toast_text);
-						textView.setText(text);
-						mNormal.setDuration(Toast.LENGTH_SHORT);
-					}
-					mNormal.show();
+					Toasty.normal(mContext,text, Toast.LENGTH_SHORT).show();
 				}catch (Exception e){
 				    e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	private static Toast mSuccess = null;
 
 	public static void success(final String text){
         if(mContext==null){
@@ -127,22 +73,13 @@ public class MyToasty {
 			@Override
 			public void run() {
 				try {
-					if (mSuccess == null) {
-						mSuccess = Toasty.success(mContext,text, Toast.LENGTH_SHORT,true);
-					} else {
-						TextView textView = (TextView)mSuccess.getView().findViewById(es.dmoral.toasty.R.id.toast_text);
-						textView.setText(text);
-						mSuccess.setDuration(Toast.LENGTH_SHORT);
-					}
-					mSuccess.show();
+					Toasty.success(mContext,text, Toast.LENGTH_SHORT,true).show();
 				}catch (Exception e){
 				    e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	private static Toast mWarning = null;
 
 	public static void warning(final String text){
         if(mContext==null){
@@ -152,14 +89,7 @@ public class MyToasty {
 			@Override
 			public void run() {
 				try {
-					if (mWarning == null) {
-						mWarning = Toasty.warning(mContext,text, Toast.LENGTH_SHORT,true);
-					} else {
-						TextView textView = (TextView)mWarning.getView().findViewById(es.dmoral.toasty.R.id.toast_text);
-						textView.setText(text);
-						mWarning.setDuration(Toast.LENGTH_SHORT);
-					}
-					mWarning.show();
+					Toasty.warning(mContext,text, Toast.LENGTH_SHORT,true).show();
 				}catch (Exception e){
 				    e.printStackTrace();
 				}
